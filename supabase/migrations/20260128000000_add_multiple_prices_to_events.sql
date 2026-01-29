995494ebecc12d42facb653f2041fc693130a299
Pedro Coelho <pedro.coelho@example.com>
2026-01-29 14:18:51 -0300

    Remove prices column migration - use existing ticket_types column
    The old migration created a prices column but we need to use the existing ticket_types column instead. All code now uses ticket_types consistently.

--- supabase/migrations/20260128000000_add_multiple_prices_to_events.sql
+++
@@ -1,8 +0,0 @@
--- Add support for multiple prices/tickets per event
-ALTER TABLE public.events ADD COLUMN prices JSONB DEFAULT '[]'::jsonb;
-
--- Create index for JSON queries
-CREATE INDEX idx_events_prices ON public.events USING GIN(prices);
-
--- Add comment for documentation
-COMMENT ON COLUMN public.events.prices IS 'Array of price/ticket types. Format: [{"name": "Pista", "price": 30}, {"name": "Camarote", "price": 60}]';
